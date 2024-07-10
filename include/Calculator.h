047ae9ad0d89ea95de4b64f9645e486c1971a11b
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2024-07-11 00:12:55 +0200

    Initial commit

---
+++ include/Calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+ public:
+  static int add(int a, int b);
+};
+
+#endif  // CALCULATOR_H
---
+++ src/Calculator.cpp
@@ -0,0 +1,5 @@
+#include "Calculator.h"
+
+int Calculator::add(int a, int b) {
+  return a + b;
+}
